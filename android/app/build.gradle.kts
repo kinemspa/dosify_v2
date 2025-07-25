4691bb48f9764e7775a902940a66ba0b0f70b128
KiNeMspA <antonycraig999@gmail.com>
2025-07-25 23:43:59 +1000

    Phase 1 build fixes: Set NDK to 27.0.12077973 and enabled core library desugaring

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,11 +8,12 @@ plugins {
 android {
     namespace = "com.kinemspa.dosify_v2"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -37,6 +38,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")  // Latest version for Java 8+ support
 }
 
 flutter {
