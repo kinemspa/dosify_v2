49785f2431b33e234ac045c125338f0854edcd1f
KiNeMspA <antonycraig999@gmail.com>
2025-07-27 11:56:48 +1000

    Phase 3 fix: Removed redundant repositories in build.gradle.kts to resolve conflict

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -8,13 +8,6 @@ buildscript {
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
