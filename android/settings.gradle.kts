e389aad44aadd63a7890bdb07477814b3774b807
KiNeMspA <antonycraig999@gmail.com>
2025-07-27 12:05:14 +1000

    Phase 3 fix: Added Flutter repo to settings.gradle.kts for plugin resolution

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,28 +1,25 @@
 pluginManagement {
-    val flutterSdkPath = run {
-        val properties = java.util.Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
-        val flutterSdkPath = properties.getProperty("flutter.sdk")
-        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-        flutterSdkPath
-    }
-
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
+    }
+    plugins {
+        id("com.android.application") version "8.7.0"
+        id("org.jetbrains.kotlin.android") version "2.0.21"
+        id("dev.flutter.flutter-gradle-plugin") version "1.0.0"
     }
 }
 
-plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.11.1" apply false
-    // START: FlutterFire Configuration
-    id("com.google.gms.google-services") version("4.3.15") apply false
-    // END: FlutterFire Configuration
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
+    }
 }
 
+rootProject.name = "dosify_v2"
 include(":app")
