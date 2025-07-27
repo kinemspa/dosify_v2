eb77aba04cb7e036d52111bc53092f04b6c440ca
KiNeMspA <antonycraig999@gmail.com>
2025-07-27 12:51:59 +1000

    Phase 3 fix: Rewrote settings.gradle.kts in Kotlin DSL for Plugin DSL compliance

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,25 +1,26 @@
 pluginManagement {
+    val flutterSdkPath: String = run {
+        val properties = java.util.Properties()
+        file("local.properties").inputStream().use { properties.load(it) }
+        val flutterSdkPath = properties.getProperty("flutter.sdk")
+            ?: error("flutter.sdk not set in local.properties")
+        flutterSdkPath
+    }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
-    }
-    plugins {
-        id("com.android.application") version "8.7.0"
-        id("org.jetbrains.kotlin.android") version "2.0.21"
-        id("dev.flutter.flutter-gradle-plugin") version "1.0.0"
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
-    }
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0" apply true
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
-rootProject.name = "dosify_v2"
 include(":app")
